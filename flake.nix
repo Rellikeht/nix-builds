a2c71fece743807a56573ecbc80656641844f468
Rellikeht <michaelhemperek@gmail.com>
2024-02-19 19:05:44 +0100

    trying to add binary minizinc

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
     minizinc.url = "./minizinc";
     chuffed.url = "./chuffed";
     breeze-hacked.url = "./breeze-hacked";
+    dwm.url = github:Rellikeht/dwm;
+    st.url = github:Rellikeht/st;
+    tabbed.url = github:Rellikeht/tabbed;
+    dmenu.url = github:Rellikeht/dmenu;
   };
 
   outputs = {
@@ -15,18 +19,40 @@
     minizinc,
     chuffed,
     breeze-hacked,
+    dwm,
+    st,
+    tabbed,
+    dmenu,
   }: let
     systems = ["x86_64-linux" "aarch64-linux"];
   in
     flakeUtils.lib.eachSystem systems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       lib = pkgs.lib;
+
+      pkgnames = [
+        minizinc
+        chuffed
+        breeze-hacked
+        dwm
+        st
+        tabbed
+        dmenu
+      ];
+
       getDef = pkg: pkg.packages.${system}.default;
+      # TODO make this work
+      # packages = builtins.map getDef pkgnames;
     in {
+      # inherit packages;
       packages = {
         minizinc = getDef minizinc;
         breeze-hacked = getDef breeze-hacked;
         chuffed = getDef chuffed;
+        dwm = getDef dwm;
+        st = getDef st;
+        tabbed = getDef tabbed;
+        dmenu = getDef dmenu;
       };
     });
 }
