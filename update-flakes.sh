db1f0a9c9908786f5528a0a058ee1ee3b75a0906
Rellikeht <michaelhemperek@gmail.com>
2024-02-27 22:16:57 +0100

    ???

--- update-flakes.sh
+++ update-flakes.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 
+NIX="nix run nixpkgs\#nix"
+alias nix=$NIX
+
 sed -nEz 's/(.*)(outputs = \{[^}]*\n)( *}:.*)/\2/p' flake.nix |
     sed '1,4d;s/^ *//;s/,.*$//' |
     xargs -d '\n' -I{} sh -c "
         ! [ -d '{}' ] && exit 0
         cd '{}'
-        nix flake update
+        rm flake.lock
+        $NIX flake update
+        $NIX flake lock
     "
 
+rm flake.lock
 nix flake update
+nix flake lock
 git add .
