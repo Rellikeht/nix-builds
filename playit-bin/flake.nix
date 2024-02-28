a78c62190de1e3be0c1b76a437a34351d3a28df2
Rellikeht <michaelhemperek@gmail.com>
2024-02-28 12:41:50 +0100

    binary playit works

--- playit-bin/flake.nix
+++ playit-bin/flake.nix
@@ -5,28 +5,30 @@
     nixpkgs.url = github:NixOS/nixpkgs;
     flakeUtils.url = github:numtide/flake-utils;
 
+    # Shit is statically linked
     pkg-linux-x64 = {
       url = "https://github.com/playit-cloud/playit-agent/releases/download/v0.15.12/playit-linux-amd64";
       type = "file";
       flake = false;
     };
 
-    # pkg-linux-x86 = {
-    #   url = "https://github.com/playit-cloud/playit-agent/releases/download/v0.15.12/playit-linux-i686";
-    #   type = "file";
-    #   flake = false;
-    # };
+    pkg-linux-x86 = {
+      url = "https://github.com/playit-cloud/playit-agent/releases/download/v0.15.12/playit-linux-i686";
+      type = "file";
+      flake = false;
+    };
 
-    # pkg-linux-a64 = {
-    #   url = "https://github.com/playit-cloud/playit-agent/releases/download/v0.15.12/playit-linux-aarch64";
-    #   type = "file";
-    #   flake = false;
-    # };
+    pkg-linux-a64 = {
+      url = "https://github.com/playit-cloud/playit-agent/releases/download/v0.15.12/playit-linux-aarch64";
+      type = "file";
+      flake = false;
+    };
 
-    # pkg-linux-a32 = {
-    #   url = "https://github.com/playit-cloud/playit-agent/releases/download/v0.15.12/playit-linux-armv7";
-    #   flake = false;
-    # };
+    pkg-linux-a32 = {
+      url = "https://github.com/playit-cloud/playit-agent/releases/download/v0.15.12/playit-linux-armv7";
+      type = "file";
+      flake = false;
+    };
   };
 
   outputs = {
@@ -34,15 +36,15 @@
     nixpkgs,
     flakeUtils,
     pkg-linux-x64,
-    # pkg-linux-x86,
-    # pkg-linux-a64,
-    # pkg-linux-a32,
+    pkg-linux-x86,
+    pkg-linux-a64,
+    pkg-linux-a32,
   }: let
     systems = {
       "x86_64-linux" = pkg-linux-x64;
-      # "i686-linux" = pkg-linux-x86;
-      # "aarch64-linux" = pkg-linux-a64;
-      # "armv7l-linux" = pkg-linux-a32;
+      "i686-linux" = pkg-linux-x86;
+      "aarch64-linux" = pkg-linux-a64;
+      "armv7l-linux" = pkg-linux-a32;
     };
 
     b = builtins;
@@ -51,54 +53,27 @@
     flib.eachSystem (b.attrNames systems) (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       lib = pkgs.lib;
-      name = "playit-bin";
+      pname = "playit-bin";
+      version = "0.15.12";
       src = systems.${system};
+      mainProgram = "playit";
     in {
       packages = {
         default = pkgs.stdenv.mkDerivation {
-          inherit name system src;
+          inherit pname version system src;
 
-          buildInputs = with pkgs; [
-            # qt6.qtbase
-            # qt6.qtwebsockets
-            # libglvnd
-            # util-linux
-
-            # gcc
-            glibc
-
-            # zlib
-            # e2fsprogs
-            # gmpxx
-          ];
-
-          nativeBuildInputs = with pkgs; [
-            autoPatchelfHook
-            coreutils
-          ];
-
-          # sourceRoot = ".";
-          # installPhase = ''
-          #   runHook preInstall
-
-          #   mkdir -p $out
-          #   cp -r ${src}/lib $out
-          #   cp -r ${src}/plugins $out
-          #   cp -r ${src}/share $out
-
-          #   cp -r ${src}/bin $out
-          #   # mkdir -p $out/bin
-          #   # find ${src}/bin -executable -type f -print0 |\
-          #   #   xargs -0 -I{} cp {} "$out/bin"
-
-          #   runHook postInstall
-          # '';
+          phases = ["installPhase"];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ${src} $out/bin/${mainProgram}
+            chmod +x $out/bin/${mainProgram}
+          '';
 
           meta = with lib; {
+            inherit mainProgram;
             homepage = "https://playit.gg/";
             description = "Binary release of playit agent";
             license = licenses.bsd2;
-            mainProgram = "playit";
             maintainers = ["Rellikeht"];
             platforms = platforms.linux; # ???
 
