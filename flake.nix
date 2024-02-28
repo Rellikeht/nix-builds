45825e304e3459d3263fbe5ce247c5d71d55c198
Rellikeht <michaelhemperek@gmail.com>
2024-02-28 15:35:19 +0100

    fucking nix, never works when it is needed

--- flake.nix
+++ flake.nix
@@ -3,19 +3,22 @@
 
   inputs = {
     flakeUtils.url = github:numtide/flake-utils;
+    nixpkgs.url = github:NixOS/nixpkgs;
+
+    dwm.url = github:Rellikeht/dwm;
+    st.url = github:Rellikeht/st;
+    tabbed.url = github:Rellikeht/tabbed;
+    dmenu.url = github:Rellikeht/dmenu;
+
     minizinc.url = "./minizinc";
     minizinc-ide-bin.url = "./minizinc-ide-bin";
     chuffed.url = "./chuffed";
     breeze-hacked.url = "./breeze-hacked";
     scheme-langserver-bin.url = "./scheme-langserver-bin";
     playit-bin.url = "./playit-bin";
-    dwm.url = github:Rellikeht/dwm;
-    st.url = github:Rellikeht/st;
-    tabbed.url = github:Rellikeht/tabbed;
-    dmenu.url = github:Rellikeht/dmenu;
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
     nixpkgs,
     flakeUtils,
@@ -30,43 +33,32 @@
     tabbed,
     dmenu,
   }: let
-    b = builtins;
     systems = ["x86_64-linux" "aarch64-linux"];
-    l64 = "x86_64-linux";
     getDefS = system: pkg: pkg.packages.${system}.default;
+    l64 = "x86_64-linux";
+
+    # TODO fuck this shit
+    pkgInputs = builtins.tail (builtins.tail (builtins.tail inputs));
 
     packagesMulti = flakeUtils.lib.eachSystem systems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      lib = pkgs.lib;
-
-      pkgnames = [
-        chuffed
-        minizinc
-        minizinc-ide-bin
-        breeze-hacked
-        scheme-langserver-bin
-        playit-bin
-
-        dwm
-        st
-        tabbed
-        dmenu
-      ];
-
-      # TODO make this work
-      # packages = builtins.map getDef pkgnames;
+      # pkgs = nixpkgs.legacyPackages.${system};
+      # lib = pkgs.lib;
       getDef = getDefS system;
+      # packages = builtins.listToAttrs (builtins.map
+      #   (name: {
+      #     inherit name;
+      #     value = getDef name;
+      #   })
+      #   pkgInputs);
     in {
       # inherit packages;
+      # });
+
       packages = {
         chuffed = getDef chuffed;
         minizinc = getDef minizinc;
         breeze-hacked = getDef breeze-hacked;
         playit-bin = getDef playit-bin;
-        dwm = getDef dwm;
-        st = getDef st;
-        tabbed = getDef tabbed;
-        dmenu = getDef dmenu;
       };
     });
 
@@ -76,11 +68,30 @@
       in {
         minizinc-ide-bin = getDef minizinc-ide-bin;
         scheme-langserver-bin = getDef scheme-langserver-bin;
+
+        dwm = getDef dwm;
+        st = getDef st;
+        tabbed = getDef tabbed;
+        dmenu = getDef dmenu;
       })
       // packagesMulti.packages.${l64};
+    packages =
+      (packagesMulti.packages)
+      // {${l64} = packagesL64;};
   in {
-    packages = packagesMulti.packages // {${l64} = packagesL64;};
+    inherit packages;
     utils = {
     };
   };
 }
+# packages = flakeUtils.lib.eachSystem systems (system: let
+#   packages = (
+#     builtins.listToAttrs (builtins.map
+#       (name: {
+#         inherit name;
+#         value = getDefS system name;
+#       })
+#       pkgInputs)
+#   );
+# in (builtins.trace packages packages));
+
