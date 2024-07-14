ea34474d08d6d31699906bb90e2cea4476e50f20
Rellikeht <michaelhemperek@gmail.com>
2024-07-14 11:25:57 +0200

    pico sdk update

--- pico-sdk/flake.nix
+++ pico-sdk/flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Raspberry Pi Pico SDK with submodules and some nice additions";
+
   inputs = {
     # {{{
     nixpkgs = {
@@ -15,6 +17,11 @@
       type = "git";
       submodules = true;
     };
+
+    pinout = {
+      url = "https://gabmus.org/pico_pinout";
+      flake = false;
+    };
   }; # }}}
 
   outputs = {
@@ -23,12 +30,13 @@
     nixpkgs,
     flake-utils,
     sdk,
+    pinout,
     # }}}
-  } @ inputs:
+  }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in rec {
-      packages = rec {
+      packages = {
         # {{{
 
         pioasm = pkgs.stdenv.mkDerivation {
@@ -58,34 +66,123 @@
           '';
         }; # }}}
 
+        pico-mount =
+          pkgs.writeScriptBin "pico-mount"
+          # {{{
+          ''
+            #!${pkgs.dash}/bin/dash
+
+            if [ "$(id -u)" != 0 ]; then
+              echo This script must be run with root priviledges
+              exit 1
+            fi
+
+            PICO_DIR=/mnt/pico
+            if [ -n "$1" ]; then
+              if [ -d "$1" ]; then
+                PICO_DIR="$1"
+              else
+                echo "$1" is not a directory
+                exit 1
+              fi
+            fi
+
+            find /dev/ -name 'sd?1' |
+              sort -r |
+              xargs -d '\n' -I{} mount "{}" "$PICO_DIR"
+          ''; # }}}
+
+        pico-load =
+          pkgs.writeScriptBin "pico-load"
+          # {{{
+          ''
+            #!${pkgs.dash}/bin/dash
+
+            if [ "$(id -u)" != 0 ]; then
+              echo This script must be run with root priviledges
+              exit 1
+            fi
+
+            if [ -z "$1" ];
+              echo You need to specify program to load
+              exit 1
+            elif [ "$1" != "uf2" ]
+              echo Is the format of "$1" correct?
+              exit 1
+            fi
+
+            PICO_DIR=/mnt/pico
+            if [ -n "$2" ]; then
+              if [ -d "$2" ]; then
+                PICO_DIR="$2"
+              else
+                echo "$2" is not a directory
+                exit 1
+              fi
+            fi
+
+            cp "$1" "$PICO_DIR"
+            sync
+            umount "$PICO_DIR"
+          ''; # }}}
+
+        pico-pinout =
+          pkgs.writeScriptBin "pico-pinout"
+          # {{{
+          ''
+            #!${pkgs.dash}/bin/dash
+
+            less --quit-if-one-screen --raw-control-chars ${pinout}
+          ''; # }}}
+
         default = pkgs.stdenv.mkDerivation {
           # {{{
           name = "pico-sdk";
           src = "${sdk}";
 
-          phases = [
-            "unpackPhase"
-            "installPhase"
-          ];
+          installPhase =
+            # {{{
+            ''
+              mkdir -p $out/lib/pico-sdk
+              cp -a * $out/lib/pico-sdk
 
-          installPhase = ''
-            mkdir -p $out/lib/pico-sdk
-            cp -a * $out/lib/pico-sdk
+              mkdir -p $out/bin
+              cp -a ${packages.elf2uf2}/bin/elf2uf2 $out/bin
+              cp -a ${packages.elf2uf2}/bin/elf2uf2 $out/lib/pico-sdk/tools/elf2uf2/
+              cp -a ${packages.pioasm}/bin/pioasm $out/bin
+              cp -a ${packages.pioasm}/bin/pioasm $out/lib/pico-sdk/tools/pioasm/
 
-            mkdir -p $out/bin
-            cp -a ${packages.elf2uf2}/bin/elf2uf2 $out/bin
-            cp -a ${packages.elf2uf2}/bin/elf2uf2 $out/lib/pico-sdk/tools/elf2uf2/
-
-            mkdir -p $out/bin
-            cp -a ${packages.pioasm}/bin/pioasm $out/bin
-            cp -a ${packages.pioasm}/bin/pioasm $out/lib/pico-sdk/tools/pioasm/
-          '';
+              cp ${packages.pico-mount}/bin/pico-mount $out/bin
+              cp ${packages.pico-load}/bin/pico-load $out/bin
+              cp ${packages.pico-pinout}/bin/pico-pinout $out/bin
+            ''; # }}}
 
           #
         }; # }}}
 
         # }}}
       };
+
+      apps = {
+        # {{{
+
+        pico-mount = {
+          type = "app";
+          program = "${packages.pico-mount}/bin/pico-mount";
+        };
+
+        pico-load = {
+          type = "app";
+          program = "${packages.pico-load}/bin/pico-load";
+        };
+
+        pico-pinout = {
+          type = "app";
+          program = "${packages.pico-pinout}/bin/pico-pinout";
+        };
+
+        #
+      }; # }}}
 
       devShells.default = with pkgs;
         mkShell {
