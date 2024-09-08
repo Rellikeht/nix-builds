aaa6d5ccc25779afdc29f78b883dd296640f160d
Rellikeht <michaelhemperek@gmail.com>
2024-09-08 18:10:30 +0200

    full deps update

--- update-flakes.sh
+++ update-flakes.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env sh
 
 find . -mindepth 2 -name 'flake.nix' -printf '%h\n' |
-    grep -Ev '' |
-    xargs -d '\n' -I{} 'sh -c "cd {} ; nix flake update"'
-
-# NIX="nix run nixpkgs\#nix"
-# alias nix=$NIX
-
-# sed -nEz 's/(.*)(outputs = \{[^}]*\n)( *}:.*)/\2/p' flake.nix |
-#     sed '1,4d;s/^ *//;s/,.*$//' |
-#     xargs -d '\n' -I{} sh -c "
-#     ! [ -d '{}' ] && exit 0
-#     cd '{}'
-#     rm flake.lock
-#     $NIX flake update
-#     $NIX flake lock
-#     "
-
-# rm flake.lock
-# nix flake update
-# nix flake lock
-# git add .
+    xargs -d '\n' -I{} sh -c 'cd "{}" ; nix flake update'
