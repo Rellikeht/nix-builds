29b7bc3fd76237ff61e8333c82eb4ac046380115
Rellikeht <michaelhemperek@gmail.com>
2024-09-08 17:59:30 +0200

    started updating deps

--- update-flakes.sh
+++ update-flakes.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env sh
 
-NIX="nix run nixpkgs\#nix"
-alias nix=$NIX
+find . -mindepth 2 -name 'flake.nix' -printf '%h\n' |
+    grep -Ev '' |
+    xargs -d '\n' -I{} 'sh -c "cd {} ; nix flake update"'
 
-sed -nEz 's/(.*)(outputs = \{[^}]*\n)( *}:.*)/\2/p' flake.nix |
-    sed '1,4d;s/^ *//;s/,.*$//' |
-    xargs -d '\n' -I{} sh -c "
-        ! [ -d '{}' ] && exit 0
-        cd '{}'
-        rm flake.lock
-        $NIX flake update
-        $NIX flake lock
-    "
+# NIX="nix run nixpkgs\#nix"
+# alias nix=$NIX
 
-rm flake.lock
-nix flake update
-nix flake lock
-git add .
+# sed -nEz 's/(.*)(outputs = \{[^}]*\n)( *}:.*)/\2/p' flake.nix |
+#     sed '1,4d;s/^ *//;s/,.*$//' |
+#     xargs -d '\n' -I{} sh -c "
+#     ! [ -d '{}' ] && exit 0
+#     cd '{}'
+#     rm flake.lock
+#     $NIX flake update
+#     $NIX flake lock
+#     "
+
+# rm flake.lock
+# nix flake update
+# nix flake lock
+# git add .
